40b2de5a15d486804fd97485e33e1335abbe394a
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-05-21 00:54:52 +0200

    showDataType method

--- js/views/effectView.js
+++ js/views/effectView.js
@@ -1,7 +1,6 @@
 export default class EffectView {
-  _letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
   _randomEffect() {
+    const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     let iterations = 0;
     const event = document.querySelector(".hack");
     const interval = setInterval(() => {
@@ -11,7 +10,7 @@ export default class EffectView {
           if (index < iterations) {
             return event.dataset.value[index];
           }
-          return this._letters[Math.floor(Math.random() * 26)];
+          return letters[Math.floor(Math.random() * 26)];
         })
         .join("");
       if (iterations >= event.dataset.value.length) clearInterval(interval);
@@ -47,7 +46,69 @@ export default class EffectView {
     obsHero.observe(bezier);
   }
 
-  showHero() {
+  showBlur() {
     this._obsHero();
+  }
+
+  showDataType() {
+    class TxtType {
+      constructor(el, toRotate, period) {
+        this.toRotate = toRotate;
+        this.el = el;
+        this.loopNum = 0;
+        this.period = parseInt(period, 10) || 2000;
+        this.txt = "";
+        this.tick();
+        this.isDeleting = false;
+      }
+      tick() {
+        const i = this.loopNum % this.toRotate.length;
+        const fullTxt = this.toRotate[i];
+
+        if (this.isDeleting) {
+          this.txt = fullTxt.substring(0, this.txt.length - 1);
+        } else {
+          this.txt = fullTxt.substring(0, this.txt.length + 1);
+        }
+
+        this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
+
+        const that = this;
+        let delta = 200 - Math.random() * 100;
+
+        if (this.isDeleting) {
+          delta /= 2;
+        }
+
+        if (!this.isDeleting && this.txt === fullTxt) {
+          delta = this.period;
+          this.isDeleting = true;
+        } else if (this.isDeleting && this.txt === "") {
+          this.isDeleting = false;
+          this.loopNum++;
+          delta = 500;
+        }
+
+        setTimeout(function () {
+          that.tick();
+        }, delta);
+      }
+    }
+
+    window.onload = function () {
+      const elements = document.getElementsByClassName("typewrite");
+      for (let i = 0; i < elements.length; i++) {
+        const toRotate = elements[i].getAttribute("data-type");
+        const period = elements[i].getAttribute("data-period");
+        if (toRotate) {
+          new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+      }
+      // INJECT CSS
+      const css = document.createElement("style");
+      css.type = "text/css";
+      css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+      document.body.appendChild(css);
+    };
   }
 }
