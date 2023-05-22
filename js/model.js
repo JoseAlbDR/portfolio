8f6b7b6f50ac75fbcba00c86c199a7258f38d99c
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-05-22 13:31:40 +0200

    Deployment

--- js/model.js
+++ js/model.js
@@ -76,7 +76,7 @@ export const techItems = [
 
 export const aboutItems = [
   {
-    imageSrc: "./assets/img/about-img.jpg",
+    imageSrc: "../assets/img/about-img.jpg",
     title: "Nunca es tarde para descubrir tu pasion.",
     text: "¡Hola! Soy J. Alberto Delgado, un estudiante principiante de desarrollo web. Mi pasión es resolver problemas a través de la programación. Actualmente, me encuentro en un bootcamp de desarrollo web full stack, ampliando mis habilidades en front-end y back-end. Mi meta es crecer como profesional en el desarrollo web y aportar mi conocimiento a proyectos futuros.",
   },
